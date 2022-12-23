59767f640e1a2810b6bfe6883a53156273fc829b
SergeAuck <sergey.auckland@gmail.com>
2022-12-23 14:50:17 +1300

    Added WINDOW

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,8 +1,25 @@
+import javax.swing.JFrame;
 
-public class Pong {
+public class Pong extends JFrame {
+	
+	public final static int WINDOW_WIDTH = 800;
+	public final static int WINDOW_HEIGHT = 600;
+	public final static String WINDOW_TITLE = "Pong";
+	public Pong() {
+			setTitle(WINDOW_TITLE);
+			setSize(WINDOW_WIDTH,WINDOW_HEIGHT);
+			setResizable(false);
+			setVisible(true);
+			setDefaultCloseOperation(EXIT_ON_CLOSE);
+		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		new Pong();
+		
+		
+		
 
 	}
 
