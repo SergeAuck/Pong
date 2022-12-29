e4c089f6d2c2c7d2999adbbc2afc8de2d926aa8b
SergeAuck <sergey.auckland@gmail.com>
2022-12-29 15:23:06 +1300

    test again

--- Pong/src/Ball.java
+++ Pong/src/Ball.java
@@ -1,7 +1,7 @@
 import java.awt.Color;
 
 public class Ball extends Sprite{
-	private final static Color BALL_COLOUR = Color.BLACK;
+	private final static Color BALL_COLOUR = Color.WHITE;
 	private final static int BALL_WIDTH = 25;
 	private final static int BALL_HEIGHT = 25;
 	//private final static int BALL_MOVEMENT_SPEED = 2;
@@ -13,6 +13,8 @@ public class Ball extends Sprite{
 		setInitialPosition(panelWidth / 2 - (getWidth() / 2), panelHeight / 2 - (getHeight() / 2));
 		resetToInitialPositions();
 	}
+	
+	//just a comment to check if new branch is working
 	
 	
 	
